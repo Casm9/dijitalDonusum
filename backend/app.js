154b64e8d1477e319b4c957e2bcfb0f6949b7c23
Mehmet Can Girgin <119158579+Casm9@users.noreply.github.com>
2024-06-12 14:01:35 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,44 +1,43 @@
 const express = require('express');
+const sequelize = require('./database/config');
+const Question = require('./database/models/Question');
 
 const app = express();
 
-const questions = [
-    {
-        question: 'İşletmenizin çalışan sayısı hangi aralıktadır?',
-        answer: true,
-        options: [
-            '0-50',
-            '51-200',
-            '201+'
-        ],
-        selected: null
-    },
-    {
-        question: 'Hangi sektörde faaliyet gösteriyorsunuz?',
-        answer: true,
-        options: [
-            'Üretim',
-            'Perakende',
-            'Hizmet',
-            'Diğer'
-        ],
-        selected: null
-    },
-    {
-        question: 'Dijital Dönüşüm sürecinizde en çok karşılaştığınız ihtiyaç hangisidir?',
-        answer: true,
-        options: [
-            'Danışman',
-            'Eğitim',
-            'Finansman'
-        ],
-        selected: null
-    }
-];
 
-app.get('/api/questions', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.json(questions);
+sequelize.sync({ force: true }).then(async () => {
+  await Question.bulkCreate([
+    {
+      question: 'İşletmenizin çalışan sayısı hangi aralıktadır?',
+      answer: 0,
+      options: ['0-50', '51-200', '201+'],
+    },
+    {
+      question: 'Hangi sektörde faaliyet gösteriyorsunuz?',
+      answer: 2,
+      options: ['Üretim', 'Perakende', 'Hizmet', 'Diğer'],
+    },
+    {
+      question: 'Dijital Dönüşüm sürecinizde en çok karşılaştığınız ihtiyaç hangisidir?',
+      answer: 1,
+      options: ['Danışman', 'Eğitim', 'Finansman'],
+    },
+  ]);
 });
 
-app.listen(3000, () => console.log('Server listening on port 3000'));
+app.get('/api/questions', async (req, res) => {
+  try {
+    const questions = await Question.findAll();
+    const formattedQuestions = questions.map(question => ({
+        ...question.toJSON(),
+        options: question.options.split(',')
+      }));
+    res.header("Access-Control-Allow-Origin", "*");
+    res.json(formattedQuestions);
+  } catch (error) {
+    console.error('Error fetching questions:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(3001, () => console.log('Server listening on port 3001'));
